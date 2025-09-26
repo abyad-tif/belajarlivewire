19c340b65a1538c6656682e4c7e58d36ea9ca6f5
abyad-tif <lapajung50@gmail.com>
2025-09-26 20:29:07 +0800

    action

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,10 +3,20 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Users extends Component
 {
+
+    public function createUser() {
+        User::create([
+            'name' => 'Nur Abyad',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('password'),
+        ]); 
+    }
+
     public function render()
     {
         return view('livewire.users', [
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,4 +1,13 @@
 <div>
     <h1>{{ $title }}</h1>
-    <p>{{ count($users) }}</p>
+    <p>Users Count: {{ count($users) }}</p>
+    <button wire:click="createUser" type="button">Create Users</button>
+
+    <hr>
+
+    <ul>
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach
+    </ul>
 </div>
